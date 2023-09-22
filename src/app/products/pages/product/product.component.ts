6ebc3d30c9123e902e464a4e15c4abe6dc2876ae
Germán <german.palencia@adentia.es>
2023-09-22 11:39:26 +0200

    ngOnChanged

--- src/app/products/pages/product/product.component.ts
+++ src/app/products/pages/product/product.component.ts
@@ -10,6 +10,7 @@ import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit,
 export class ProductComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy{
 
   public isProductVisible: boolean = false;
+  public currentPrice: number = 10;
 
   constructor () {
     console.log('Constructor');
@@ -45,6 +46,10 @@ export class ProductComponent implements OnInit, OnChanges, DoCheck, AfterConten
 
   ngOnDestroy(): void {
     console.log('ngOnDestroy');
+  }
+
+  increasePrice() {
+    this.currentPrice++;
   }
 
 }
